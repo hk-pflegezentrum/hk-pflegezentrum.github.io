fa10fcfc83a8bc42b993c89efb649fee6169780c
Tobias B. Grun <tobiasgrun@gmail.com>
2025-08-30 18:57:22 +0200

    update

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+const toggleBtn = document.querySelector('.menu-toggle');
+const navOverlay = document.querySelector('.nav-overlay');
+
+toggleBtn.addEventListener('click', () => {
+  navOverlay.classList.toggle('active');
+});
