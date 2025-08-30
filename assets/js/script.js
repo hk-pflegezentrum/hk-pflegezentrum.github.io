93f843e547540a2f1f7c8fa2999a245757f370c2
Tobias B. Grun <tobiasgrun@gmail.com>
2025-08-30 19:43:07 +0200

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,6 @@ const navOverlay = document.querySelector('.nav-overlay');
 
 toggleBtn.addEventListener('click', () => {
   navOverlay.classList.toggle('active');
+  toggleBtn.textContent = 
+    navOverlay.classList.contains('active') ? '✖' : '☰';
 });
